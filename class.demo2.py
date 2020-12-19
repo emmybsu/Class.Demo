decd15d57658cd6645e7cfeff826a77d7bb2d589
emmybsu <emjcruzmsnrn@gmail.com>
2020-12-19 14:45:13 -0500

    saved both from VS Code

--- class.demo2.py
+++ class.demo2.py
@@ -2,3 +2,12 @@ print stuff
 a+b = callable
 stuff
 stuff
+stuff
+
+hi World
+all is while no 
+
+no pandemic:
+    pass
+else:
+    pass
--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 print('Hello World!')
+
+def AgeAnalysis(a,b):
+    print(a,b)
+
+def age(c,d):
+    print(c,d)
